9655271afd917b224a23f4affc6f8a0a1053faab
Yiming Zhu <1309563431@qq.com>
2024-09-07 09:10:59 +0800

    modify the save_pcd function from list to numpy operation

--- python/yolosense.py
+++ python/yolosense.py
@@ -41,25 +41,37 @@ def get_aligned_images(frame):
 def save_pcd(intrinsics, units, depth_image, box):
     print("saving to output.pcd ...")
     # 创建空的点云列表
-    points = []
+    # points = []
     
     pad_y = 20
     pad_x = 20
 
+    x_1 = box[0] - pad_x
+    x_2 = box[2] + pad_x
+    y_1 = box[1] - pad_y
+    y_2 = box[3] + pad_y
+
     # 遍历图像的每个像素
-    for v in range(box[1] - pad_y, box[3] + pad_y):
-        for u in range(box[0] - pad_x, box[2] + pad_x):
-            z = depth_image[v, u] * units  # 深度值转换为米
-            if z > 0:  # 排除无效点
-                x = (u - intrinsics.ppx) / intrinsics.fx * z
-                y = (v - intrinsics.ppy) / intrinsics.fy * z
-                points.append([x, y, z])
+    U = np.multiply(np.arange(x_1, x_2), np.ones((y_2 - y_1, 1))).astype(np.int16)
+    V = np.multiply(np.arange(y_1, y_2).reshape(-1, 1), np.ones((1, x_2 - x_1))).astype(np.int16)
+
+    Z = depth_image[y_1:y_2, x_1:x_2] *units
+    X = (U - intrinsics.ppx) / intrinsics.fx * Z
+    Y = (V - intrinsics.ppy) / intrinsics.fy * Z
+    points = np.concatenate((X.reshape(-1, 1), Y.reshape(-1, 1), Z.reshape(-1, 1)), axis=1)
+    # for v in range(box[1] - pad_y, box[3] + pad_y):
+    #     for u in range(box[0] - pad_x, box[2] + pad_x):
+    #         z = depth_image[v, u] * units  # 深度值转换为米
+    #         if z > 0:  # 排除无效点
+    #             x = (u - intrinsics.ppx) / intrinsics.fx * z
+    #             y = (v - intrinsics.ppy) / intrinsics.fy * z
+    #             points.append([x, y, z])
 
     # 将点和颜色数据转换为Open3D点云格式
     pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(np.array(points))
+    pcd.points = o3d.utility.Vector3dVector(points)
 
-    # 保存点云为PLY文件
+    # 保存点云为PCD文件
     o3d.io.write_point_cloud("output.pcd", pcd)
     print("Done! output.pcd")
 
@@ -91,7 +103,7 @@ class Var2Py(ctypes.Structure):
  
 if __name__ == '__main__':
     # load weights
-    model = YOLO("best.pt") 
+    model = YOLO("./weights/best.pt") 
 
     # load shared library from cpp
     so = ctypes.CDLL("/home/zju/Yolov8/build/libtemplate_alignment.so")
